22b79a499c7a81b73af516a3c11330c1c34d5259
Josh Martin <Josh.Martin@samtec.com>
2018-04-17 15:51:08 -0400

    wrapped up functionality/styling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,5 @@
 //Click X to remove the list item
-$('span').on('click', function(event){
+$('ul').on('click', 'span', function(event){
     $(this).parent().fadeOut(500, function(){
         //removes the parent of the span (li)
         $(this).remove();
@@ -9,10 +9,24 @@ $('span').on('click', function(event){
 });
 
 //Cross off todos by clicking.
-$('li').on('click', function(){
+//when an li is clicked inside a ul run this code
+//li that may or may not be on the page when it loaded but the ul
+//was definitely on the page when it loaded.
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('completed');
 });
 
-$("input[type='text']").keypress(function(){
-    console.log('key press');
+$("input[type='text']").keypress(function(event){
+    //check for character code 13 (enter key)
+    if(event.which === 13){
+        //takes new todo from input
+      var todo = $(this).val();
+      $(this).val('');
+      //create new li and add to ul
+      $('ul').append('<li><span><i class="fas fa-minus-circle"></i></span> ' + todo + '</li>');
+    }
+});
+
+$('#toggle-form').click(function(){
+    $("input[type='text']").fadeToggle();
 });
